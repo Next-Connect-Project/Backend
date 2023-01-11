9fab59c8e7302cca5271694bb887163cbf1da699
heenahan <mby1997s@gmail.com>
2023-01-11 16:03:02 +0900

    feat: swagger, cors 설정

--- src/main/java/com/project/unigram/MainController.java
+++ src/main/java/com/project/unigram/MainController.java
@@ -3,13 +3,15 @@ package com.project.unigram;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class MainController {
 
     @GetMapping
-    public ResponseEntity<String> isMain() {
-        String where = "is SPRING server on 8080 port";
+    public ResponseEntity<String> sample() {
+        String where = "running SPRING server on 8080 port";
         return ResponseEntity.ok(where);
     }
 
