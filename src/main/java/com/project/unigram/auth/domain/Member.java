389543cc5fd61c9575b5c3447a1fa8b86bbab783
heenahan <mby1997s@gmail.com>
2023-02-02 14:25:49 +0900

    feat: member 엔티티 추가

---
+++ src/main/java/com/project/unigram/auth/domain/Member.java
@@ -0,0 +1,24 @@
+package com.project.unigram.auth.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+	@Id
+	@Column(name = "member_id")
+	private Long id; // 네이버에서 발급받는 고유 아이디
+	
+	private String name;
+	
+	private String email;
+	
+}
