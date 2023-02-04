0b0e36ced2e3f813b9a76d6f760ea35a18d12345
heenahan <mby1997s@gmail.com>
2023-02-05 00:19:04 +0900

    #2 fix : deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,40 +1,36 @@
-# project > scripts > deploy.sh
 #!/bin/bash
 
 REPOSITORY=/home/ubuntu/app/zip
-DEPLOY_DIRECTORY=/home/ubuntu/deploy
-PROJECT_NAME=unigram-webservice
+PROJECT_NAME=unigram-webserver
+
+cd $REPOSITORY/$PROJECT_NAME
+
+echo "> Git PULL"
+git pull
+echo "프로젝트 빌드 시작"
+
+./gradlew build
+
+echo "> /app/zip 디렉토리로 이동 "
+cd $REPOSITORY
 
 echo "> Build 파일 복사"
-
-cp $DEPLOY_DIRECTORY/$PROJECT_NAME/*.jar $REPOSITORY/$PROJECT_NAME/
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
 
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
-
-echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-
+echo "현재 구동중인 애플리케이션 pid = $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
-   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-   echo "> kill -15 $CURRENT_PID"
-   kill -15 $CURRENT_PID
-   sleep 5
+  echo"> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 
-JAR_NAME=$(ls -tr $REPOSITORY/$PROJECT_NAME/*.jar | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
-rm $REPOSITORY/$PROJECT_NAME/nohup.out
-
-nohup java -jar -Dspring.profiles.active=alpha -Dfile.encoding=UTF-8  $JAR_NAME > $REPOSITORY/$PROJECT_NAME/nohup.out 2>&1 &
+echo "> JAR NAME: $JAR_NAME"
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
