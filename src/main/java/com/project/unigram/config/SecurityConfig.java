c58f87b5099ab6ae7cfbbaa0446cdcc0ddf097f1
heenahan <mby1997s@gmail.com>
2023-04-13 19:21:31 +0900

    #5 fix : 시큐리티에서 swagger-ui가 동작하도록 설정 바꿈

--- src/main/java/com/project/unigram/config/SecurityConfig.java
+++ src/main/java/com/project/unigram/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.project.unigram.config;
 
+import com.project.unigram.auth.security.JWTAuthenticationFilter;
 import com.project.unigram.auth.security.TokenGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,16 +20,42 @@ public class SecurityConfig {
 	private final TokenGenerator tokenGenerator;
 	
 	@Bean
+	public WebSecurityCustomizer configure() {
+		return (web) -> web.ignoring().mvcMatchers(
+				/* swagger v2 */
+				"/v2/api-docs",
+				"/swagger-resources",
+				"/swagger-resources/**",
+				"/configuration/ui",
+				"/configuration/security",
+				"/swagger-ui.html",
+				"/webjars/**",
+				/* swagger v3 */
+				"/v3/api-docs/**",
+				"/swagger-ui/**"
+		);
+	}
+	
+	
+	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-				.csrf().disable()
-				.sessionManagement().disable()
-				.authorizeRequests()
-				.antMatchers("/api/v1/auth/**").permitAll()
-				.anyRequest().authenticated()
+				.cors()
 				.and()
-				.httpBasic().disable();
+				.csrf().disable()
+				.httpBasic().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.authorizeRequests()
+				.antMatchers("/api/auth/login/naver").permitAll()
+				.anyRequest().authenticated();
+		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 		return http.build();
+	}
+	
+	@Bean
+	public JWTAuthenticationFilter jwtAuthenticationFilter() {
+		return new JWTAuthenticationFilter(tokenGenerator);
 	}
 
 }
