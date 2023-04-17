7b57494ef719c8958bc5fe9e695c31405b803f61
heenahan <mby1997s@gmail.com>
2023-04-17 19:13:04 +0900

    #5 feat : 헤더에 토큰 없을 시 에러 발생

--- src/main/java/com/project/unigram/auth/security/JWTAuthenticationFilter.java
+++ src/main/java/com/project/unigram/auth/security/JWTAuthenticationFilter.java
@@ -3,8 +3,11 @@ package com.project.unigram.auth.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.unigram.auth.domain.Token;
 import com.project.unigram.auth.domain.Type;
+import com.project.unigram.auth.exception.AuthErrorCode;
 import com.project.unigram.auth.exception.TokenInvalidException;
+import com.project.unigram.global.dto.ErrorCode;
 import com.project.unigram.global.dto.ResponseError;
+import com.project.unigram.global.exception.ServerException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
@@ -12,6 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.print.DocFlavor;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -23,11 +27,19 @@ import java.io.OutputStream;
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
 	
 	private final TokenGenerator tokenGenerator;
+	private ObjectMapper ob = new ObjectMapper();
 	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 		String token = getJWTFromRequest(request);
-
+		String uri = request.getRequestURI();
+		
+		try {
+			if (!uri.equals("/api/auth/login/naver") && !StringUtils.hasText(token)) throw new TokenInvalidException("헤더에 토큰을 넣어주세요", AuthErrorCode.NOT_EXISTS);
+		} catch (TokenInvalidException e) {
+			sendError(response, 401, e.getErrorCode(), e.getMessage());
+		}
+		
 		if (StringUtils.hasText(token)) {
 			try {
 				Type type = request.getRequestURI().equals("/api/auth/reissue") ? Type.RTK : Type.ATK;
@@ -46,15 +58,9 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
 				Authentication authentication = tokenGenerator.getAuthentication(authToken, type);
 				SecurityContextHolder.getContext().setAuthentication(authentication);
 			} catch (TokenInvalidException e) {
-				ObjectMapper ob = new ObjectMapper();
-				OutputStream out = response.getOutputStream();
-				
-				response.setStatus(401);
-				response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-				ResponseError error = new ResponseError(401, e.getLocalizedMessage());
-				
-				ob.writeValue(out, error);
-				out.flush();
+				sendError(response, 401, e.getErrorCode(), e.getMessage());
+			} catch (ServerException e) {
+				sendError(response, 500, e.getErrorCode(), e.getMessage());
 			}
 		}
 		
@@ -69,6 +75,17 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
 			return bearerToken.substring(7, bearerToken.length());
 		}
 		return null;
+	}
+	
+	private void sendError(HttpServletResponse response, int status, ErrorCode code, String message) throws IOException {
+		OutputStream out = response.getOutputStream();
+		
+		response.setStatus(status);
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		ResponseError error = new ResponseError(code, message);
+		
+		ob.writeValue(out, error);
+		out.flush();
 	}
 	
 }
