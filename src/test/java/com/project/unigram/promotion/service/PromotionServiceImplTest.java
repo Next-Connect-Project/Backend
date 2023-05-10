c6524027789dae5650a49a0dc549773c408c2d22
songhee1 <ghenrhkwk88@naver.com>
2023-05-10 15:31:41 +0900

    #21 테스트 코드 수정

--- src/test/java/com/project/unigram/promotion/service/PromotionServiceImplTest.java
+++ src/test/java/com/project/unigram/promotion/service/PromotionServiceImplTest.java
@@ -1,34 +1,16 @@
 package com.project.unigram.promotion.service;
-
-import com.project.unigram.auth.domain.Member;
-import com.project.unigram.auth.domain.Role;
-import com.project.unigram.auth.repository.MemberRepository;
-import com.project.unigram.auth.service.MemberService;
-import com.project.unigram.promotion.domain.Promotion;
-import com.project.unigram.promotion.dto.PromotionCreateDto;
 import com.project.unigram.promotion.dto.PromotionDto;
 import com.project.unigram.promotion.repository.PromotionRepository;
-import org.assertj.core.api.Assertions;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
-
-import static com.project.unigram.auth.domain.QMember.member;
-import static org.junit.jupiter.api.Assertions.*;
-
 class PromotionServiceImplTest {
 
     @Autowired
     private PromotionRepository promotionRepository;
-
-    @BeforeEach
-    public void clean(){
-        promotionRepository.deleteAll();
-    }
+    
 
     @DisplayName("Posts register test")
     @Test
