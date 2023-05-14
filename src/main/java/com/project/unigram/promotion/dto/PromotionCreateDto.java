da7596e95a1f2a41ba14f7941744bbd2126f6227
songhee1 <ghenrhkwk88@naver.com>
2023-05-14 21:15:50 +0900

    #26 Valid 어노테이션 활용

--- src/main/java/com/project/unigram/promotion/dto/PromotionCreateDto.java
+++ src/main/java/com/project/unigram/promotion/dto/PromotionCreateDto.java
@@ -1,11 +1,13 @@
 package com.project.unigram.promotion.dto;
 
+import com.project.unigram.global.exception.ValidationErrorCode;
 import com.project.unigram.promotion.domain.Promotion;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
@@ -19,8 +21,11 @@ import java.util.Date;
 
 public class PromotionCreateDto {
 
+    @NotBlank(message = "NOT_EMPTY:홍보 게시글의 제목이 비어있습니다.")
     private String title;
+    @NotBlank(message = "NOT_EMPTY:홍보 게시글의 내용이 비어있습니다.")
     private String content;
+    @NotBlank(message = "NOT_EMPTY:홍보 게시글의 요약정보가 비어있습니다.")
     private String abstractContent;
 
     public PromotionCreateDto toDto(Promotion promotion){
